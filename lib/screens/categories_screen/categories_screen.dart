35c9182aff7765da4c2a1de09e4b5339eeb47df2
Mikael Micheline <105074940+mikaelmicheline@users.noreply.github.com>
2025-09-29 18:30:30 -0300

    add meals screen

--- lib/screens/categories_screen/categories_screen.dart
+++ lib/screens/categories_screen/categories_screen.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category_model.dart';
 import 'package:meals_app/screens/categories_screen/widgets/category_grid_item.dart';
+import 'package:meals_app/screens/meals_screen/meals_screen.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, CategoryModel category) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (ctx) => MealsScreen(category: category)));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +28,12 @@ class CategoriesScreen extends StatelessWidget {
         ),
         children: [
           for (final category in availableCategories)
-            CategoryGridItem(category: category)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
         ],
       ),
     );
---
+++ lib/screens/meals_screen/meals_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category_model.dart';
+import 'package:meals_app/models/meal_model.dart';
+import 'package:meals_app/screens/meals_screen/widgets/meal_item.dart';
+
+class MealsScreen extends StatelessWidget {
+  MealsScreen({super.key, required this.category})
+      : meals = availableMeals
+            .where((meal) => meal.categories.contains(category.id))
+            .toList();
+
+  final CategoryModel category;
+  final List<MealModel> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = meals.isEmpty
+        ? Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  "Uh oh... nothing here!",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineLarge!
+                      .copyWith(color: Theme.of(context).colorScheme.onSurface),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  "Try selecting a different category!",
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyLarge!
+                      .copyWith(color: Theme.of(context).colorScheme.onSurface),
+                )
+              ],
+            ),
+          )
+        : ListView.builder(
+            itemCount: meals.length,
+            itemBuilder: (ctx, index) => MealItem(meal: meals[index]),
+          );
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(category.title),
+        ),
+        body: content);
+  }
+}
